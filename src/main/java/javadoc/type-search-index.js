0e1a92f067a35760ce513a8dbc57aca8ac6dc9ba
Evgeniy <e.vasiliev. 86@mail.ru>
2023-10-24 22:26:29 +0300

    create javadoc

---
+++ src/main/java/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Director"},{"p":"org.example","l":"Employee"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Methods"}];updateSearchResults();
